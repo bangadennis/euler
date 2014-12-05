0ebb28466d66cbda324d77a27972726da5da0299
banga <dennisbanga@gmail.com>
2014-12-05 15:32:47 +0300

    added to find product factors of number

--- primelargest.py
+++ primelargest.py
@@ -1,19 +1,39 @@
 
 def primefactors(number):
 	primelist=[]
-	count=0
-	for i in range(1, number):
-		for x in range(1,i):
-			if i%x==0 and x<i and x>1:
+	primefactors=[]
+	n=1
+	while(n<=number):
+		print "===>>>>>>>>", n
+		if number%n==0:
+			print "added<<<=======", n
+			primelist.append(n)
+		n=n+1
+
+	
+	for num in primelist:
+		count=0
+		print "===>",num
+		for i in range(1, num):
+			print "<===",num
+			if num%i==0 and i<num and i>1:
 				count=-1
-		if count!=-1:
-			primelist.append(i)
-			
-	return primelist
+				break;
+		if count==0:
+			primefactors.append(num)
+
+	return primefactors
+				
+	
+
 
 
 if __name__ == '__main__':
-	resultList=primefactors(44)
+	resultList=primefactors(600851475143)
 	for i in resultList:
 		print "\n", i
-	print max(resultList)
+	if not resultList:
+		print "AM Empty"
+	else:
+		print "Maximum Value of the primefactors is: "
+		print max(resultList)
